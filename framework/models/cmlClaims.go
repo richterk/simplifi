a068c83b9e250aa0b233312f9f4098eaeb8f20df
Kyle Richter <kyler@campfire-software.com>
2018-04-14 23:40:36 -0500

    - Adding the claims object for parsing claims from a jwt to the framework package.

---
+++ framework/models/cmlClaims.go
@@ -0,0 +1,48 @@
+package framework
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+// CmlClaims are the claims for JWT tokens
+type CmlClaims struct {
+	ID        string
+	Username  string
+	Roles     []string
+	ExpiresAt int64
+}
+
+// Valid checks whether the claims the user has are valid.  Both ROLE_ADMIN and ROLE_USER are valid.
+func (claims CmlClaims) Valid() error {
+	if !(time.Now().Unix() <= int64(claims.ExpiresAt)) {
+		return AuthError{When: time.Now(), What: "token has expired"}
+	}
+	fmt.Printf("Claims: %+v \n\n", claims)
+	for i := 0; i < len(claims.Roles); i++ {
+		realRole := strings.Replace(claims.Roles[i], "\"", "", 2) // So stupid that this has to be done.
+		fmt.Printf("Role: %+v, %v - Username: %s \n\n", claims.Roles[i], realRole == "ROLE_ADMIN", claims.Username)
+		if realRole == "ROLE_ADMIN" {
+			fmt.Printf("Admin...\n")
+			return nil
+		}
+
+		if realRole == "ROLE_USER" {
+			fmt.Printf("User...\n")
+			return nil
+		}
+	}
+
+	fmt.Printf("Error...\n")
+	return AuthError{When: time.Now(), What: "You don't have the required role"}
+}
+
+type AuthError struct {
+	When time.Time
+	What string
+}
+
+func (e AuthError) Error() string {
+	return fmt.Sprintf("%v: %v", e.When, e.What)
+}
