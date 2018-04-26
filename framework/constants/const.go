e0a88927f00ca9cc81917d89cd563132ec76e52f
Kyle Richter <kyle@eventrad.io>
2018-04-25 22:08:14 -0500

    Adding "Hydrate Coin" const

--- framework/constants/const.go
+++ framework/constants/const.go
@@ -1,6 +1,7 @@
 package constants
 
 const (
-	CONTROLLER_COLLECTION_COIN string = "coin"
-	CONTROLLER_COLLECTION_USER string = "user"
+	CONTROLLER_COLLECTION_HYDRATE_COIN string = "coin"
+	CONTROLLER_COLLECTION_COIN         string = "coin"
+	CONTROLLER_COLLECTION_USER         string = "user"
 )
