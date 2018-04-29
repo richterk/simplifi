037b2a1cccbb0e686aec3f764010a07fa51ee1e3
Kyle Richter <kyler@campfire-software.com>
2018-04-29 00:05:13 -0500

    Adding graph collection

--- framework/constants/const.go
+++ framework/constants/const.go
@@ -2,6 +2,7 @@ package constants
 
 const (
 	CONTROLLER_COLLECTION_HYDRATE_COIN string = "coin"
+	CONTROLLER_COLLECTION_GRAPH        string = "graphData"
 	CONTROLLER_COLLECTION_COIN         string = "coin"
 	CONTROLLER_COLLECTION_USER         string = "user"
 )
