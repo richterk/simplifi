c8771903d9de2752716369619c54aa4b6a811896
Kyle Richter <kyletrichter@yahoo.com>
2018-06-08 22:10:46 -0500

    Update middleware.go

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -5,10 +5,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"time"
-
 	"github.com/gin-contrib/cors"
-
 	"github.com/gin-gonic/gin"
+	"github.com/richterk/simplifi-framework/framework"
 )
 
 // LogProperties logs out all of the application properties
