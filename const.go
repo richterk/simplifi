86350de22d68634aa3c009a7714a98d1334b0426
Kyle Richter <kyler@campfire-software.com>
2018-08-24 12:50:49 -0500

    adding item

--- const.go
+++ const.go
@@ -1,8 +1,6 @@
 package simplifiFramework
 
 const (
-	CONTROLLER_COLLECTION_HYDRATE_COIN string = "coin"
-	CONTROLLER_COLLECTION_GRAPH        string = "graphData"
-	CONTROLLER_COLLECTION_COIN         string = "coin"
-	CONTROLLER_COLLECTION_USER         string = "user"
+	CONTROLLER_COLLECTION_USER string = "user"
+	CONTROLLER_COLLECTION_ITEM string = "item"
 )
