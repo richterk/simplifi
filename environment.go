ace4dc09da7dc792b2f503378f45ea48937a4631
Kyle Richter <kyler@campfire-software.com>
2018-09-01 20:52:45 -0500

    Fixing a typo

--- environment.go
+++ environment.go
@@ -20,6 +20,41 @@ type IgnitionEngineEnvironment struct {
 }
 
 // InitDB - DB Stuff
+func (env *IgnitionEngineEnvironment) InitDB(props *Properties) {
+	appProperties := props
+	//		session, err := mgo.Dial(appProperties.DB.Host)
+	// NEW DB LOGIC
+	uri := strings.Replace(appProperties.DB.Host, "?ssl=true&", "?", 1)
+	tlsConfig := &tls.Config{}
+	tlsConfig.InsecureSkipVerify = true
+
+	dialInfo, err := mgo.ParseURL(uri)
+
+	if err != nil {
+		panic(err)
+	}
+
+	dialInfo.DialServer = func(addr *mgo.ServerAddr) (net.Conn, error) {
+		conn, err := tls.Dial("tcp", addr.String(), tlsConfig)
+		return conn, err
+	}
+
+	session, err := mgo.DialWithInfo(dialInfo)
+	if err != nil {
+		fmt.Println("Failed to connect: ", err)
+		panic(err)
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	// END NEW DB LOGIC
+
+	env.DB = session
+}
+
+// InitDB - DB Stuff
 func (env *Environment) InitDB(props *Properties) {
 	appProperties := props
 	//		session, err := mgo.Dial(appProperties.DB.Host)
