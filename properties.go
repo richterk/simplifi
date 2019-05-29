141bba14aaaed7f2428c5f8164d70bc62bacf8a8
Kyle Richter <kyler@campfire-software.com>
2019-05-29 17:53:14 -0500

    Updating properties

--- properties.go
+++ properties.go
@@ -7,13 +7,21 @@ type DatabaseProperties struct {
 	Password string `json:"password"`
 }
 
+type Amqp struct{
+	Username	string	`json:"username"`
+    Password	string 	`json:"password"`
+	Host		string 	`json:"host"`
+    Port		int		`json:"port"`
+}
+
 // Properties for the API
 type Properties struct {
-	Environment    string             `json:"environment"`
-	DB             DatabaseProperties `json:"db"`
-	Port           int                `json:"port"`
-	JwtSecret      string             `json:"jwtSecret"`
-	GithubAPIToken string             `json:"githubAPIToken"`
+	Environment    	string             	`json:"environment"`
+	DB             	DatabaseProperties 	`json:"db"`
+	Port           	int                	`json:"port"`
+	JwtSecret      	string             	`json:"jwtSecret"`
+	GithubAPIToken 	string             	`json:"githubAPIToken"`
+	Amqp			Amqp				`json:"amqp"`
 }
 
 // IgnitionEngineProperties are the properties that exist in an IgnitionEngine Application
