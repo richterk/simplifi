13436aef1f822c026ad7e67ef639b205d3a10803
Kyle Richter <kyler@campfire-software.com>
2019-11-30 16:49:00 -0600

    Adding a version 0 version

---
+++ v0/middleware.go
@@ -0,0 +1,74 @@
+package simplifiFramework
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+// LogProperties logs out all of the application properties
+func LogProperties(props *Properties) gin.HandlerFunc {
+	fmt.Println("Props...")
+	return func(c *gin.Context) {
+		fmt.Println("Props handler...")
+		_, exists := c.Get("Properties")
+		if !exists {
+			c.Set("Properties", props)
+		}
+		c.Next()
+	}
+}
+
+// LoadPropertiesByEnv loads the properties file based on the environment
+func LoadPropertiesByEnv(env string) *Properties {
+	propertiesReader := fmt.Sprintf("./config/config.%s.json", env)
+	bytes, err := ioutil.ReadFile(propertiesReader)
+
+	if err != nil {
+		panic(err)
+	}
+	var props Properties
+
+	err = json.Unmarshal(bytes, &props)
+	if err != nil {
+		panic(err)
+	}
+
+	return &props
+
+}
+
+func LoadIgnitionEnginePropertiesByEnv(env string) *IgnitionEngineProperties {
+	propertiesReader := fmt.Sprintf("./config/config.%s.json", env)
+	bytes, err := ioutil.ReadFile(propertiesReader)
+
+	if err != nil {
+		panic(err)
+	}
+	var props IgnitionEngineProperties
+
+	err = json.Unmarshal(bytes, &props)
+	if err != nil {
+		panic(err)
+	}
+
+	return &props
+
+}
+
+func CorsConfig() gin.HandlerFunc {
+
+	fmt.Println("Creating config...")
+	return cors.New(cors.Config{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Access-Control-Allow-Origin, Access-Control-Allow-Header, Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization", "Accept", "Origin", "Cache-Control", "X-Requested-With"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	})
+}
---
+++ v0/properties.go
@@ -0,0 +1,34 @@
+package simplifiFramework
+
+type DatabaseProperties struct {
+	Host     string `json:"host"`
+	Database string `json:"database"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+}
+
+type Amqp struct{
+	Username	string	`json:"username"`
+    Password	string 	`json:"password"`
+	Host		string 	`json:"host"`
+    Port		int		`json:"port"`
+}
+
+// Properties for the API
+type Properties struct {
+	Environment    	string             	`json:"environment"`
+	DB             	DatabaseProperties 	`json:"db"`
+	Port           	int                	`json:"port"`
+	JwtSecret      	string             	`json:"jwtSecret"`
+	GithubAPIToken 	string             	`json:"githubAPIToken"`
+	Amqp			Amqp				`json:"amqp"`
+}
+
+// IgnitionEngineProperties are the properties that exist in an IgnitionEngine Application
+type IgnitionEngineProperties struct {
+	Environment       string             `json:"environment"`
+	DB                DatabaseProperties `json:"db"`
+	ClientInstanceKey string             `json:"client_key"`
+	TargetChannel     string             `json:"target_channel"`
+	ApplicationName   string             `json:"app_name"`
+}
