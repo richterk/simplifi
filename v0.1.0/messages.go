6b8ce780fe483af739fedc5d2c2dadfa602ae905
Kyle Richter <kyler@campfire-software.com>
2019-11-30 15:27:54 -0600

    Upgrading to modules

---
+++ v0.1.0/messages.go
@@ -0,0 +1,8 @@
+package simplifiFramework
+
+const (
+	FAILED_TO_CONNECT_TO_DATABASE string = "Failed to connect to the user database"
+	NOT_FOUND                     string = "NOT FOUND"
+	UNAUTHORIZED                  string = "Unauthorized"
+	INVALID_USER_ID               string = "Invalid User id"
+)
---
+++ v0.1.0/simplifi.go
@@ -0,0 +1,3 @@
+package simplifiFramework
+
+func Something() {}
