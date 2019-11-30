f6e26f2280dc822bcc942602c4690e6bf147e27c
Kyle Richter <kyler@campfire-software.com>
2019-11-30 15:01:21 -0600

    Adding a module definition and adding a new constant

--- const.go
+++ const.go
@@ -1,6 +1,7 @@
 package simplifiFramework
 
 const (
-	CONTROLLER_COLLECTION_USER string = "user"
-	CONTROLLER_COLLECTION_ITEM string = "item"
+	CONTROLLER_COLLECTION_USER        string = "user"
+	CONTROLLER_COLLECTION_ITEM        string = "item"
+	CONTROLLER_COLLECTION_LEADERBOARD string = "leaderboard"
 )
